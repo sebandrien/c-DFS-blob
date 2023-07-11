f528e930fcb7568a5c2fc6295b6bc951935fe4fe
Sebastian <129107648+sebandrien@users.noreply.github.com>
2023-07-11 15:51:51 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,88 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int blobDetect(int ** picture, int x, int y, int size);
+
+// Generate a picture with random 1s and 0s
+void generatePicture(int ** picture, int size) {
+    for (int r = 0; r < size; ++r)
+        for (int c = 0; c < size; ++c)
+            picture[r][c] = rand() % 2; // generate random number between 0 and 1
+}
+
+// Print the picture
+void printPicture(int ** picture, int size) {
+    for (int r = 0; r < size; ++r) {
+        for (int c = 0; c < size; ++c) {
+            printf("%d ", picture[r][c]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+  int seed;
+  printf("Enter seed: ");
+  scanf("%d", &seed);
+  srand(seed); // set seed
+
+  printf("Enter the size of the picture, r * c: ");
+  int size;
+  scanf("%d", &size);
+
+  // Allocate memory for the picture
+  int ** picture = (int **) malloc(size * sizeof(int *));
+  if(picture == NULL) {
+      printf("Failed to allocate memory!\n");
+      return 1;
+  }
+  for (int i = 0; i < size; ++i) {
+    picture[i] = (int *) malloc(size * sizeof(int));
+    if(picture[i] == NULL) {
+        printf("Failed to allocate memory!\n");
+        return 1;
+    }
+  }
+
+  for (int test = 1; test <= 5; ++test) {
+    printf("Test %d\n", test);
+    generatePicture(picture, size);
+
+    printf("Generated picture:\n");
+    printPicture(picture, size);
+
+    int numblobs = 0;
+
+    for (int r = 0; r < size; ++r)
+      for (int c = 0; c < size; ++c)
+        if (blobDetect(picture, r, c, size) > 0)
+          ++numblobs;
+    printf("There are %d blob(s) in the picture.\n", numblobs);
+  }
+
+  // Free the allocated memory
+  for (int i = 0; i < size; ++i)
+    free(picture[i]);
+  free(picture);
+  return 0;
+}
+
+int blobDetect(int ** picture, int x, int y, int size) {
+  if (x < 0 || x >= size || y < 0 || y >= size) {
+    return 0;
+  }
+
+  if (picture[x][y] == 0) {
+    return 0;
+  } else {
+    picture[x][y] = 0;
+    return 1 + blobDetect(picture, x - 1, y - 1, size) +
+           blobDetect(picture, x + 1, y - 1, size) +
+           blobDetect(picture, x + 1, y, size) +
+           blobDetect(picture, x + 1, y + 1, size) +
+           blobDetect(picture, x - 1, y, size) +
+           blobDetect(picture, x - 1, y + 1, size) +
+           blobDetect(picture, x, y - 1, size) +
+           blobDetect(picture, x, y + 1, size);
+  }
+}
